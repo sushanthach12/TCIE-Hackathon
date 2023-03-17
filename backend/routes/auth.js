24ab76a13e590edff30b4e4356129b9bcc07fa47
Sushanth Acharya <84125038+sushanthach12@users.noreply.github.com>
2023-03-17 10:56:10 +0530

    auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -49,4 +49,67 @@ router.post('/signup', [
 })
 
 
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ err: errors.array() })
+    }
+
+    try {
+
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with this email already exists!" })
+        }
+
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.AES_SEC);
+        const password = hashedPassword.toString(CryptoJS.enc.Utf8)
+
+        if (password !== req.body.password) {
+            return res.status(400).json({ error: "Please enter correct credentials" })
+        }
+
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+
+        const authToken = jwt.sign(data, JWT_SECRET, { expiresIn: 60 * 60 * 24 * 30 });
+
+        return res.json({ "Success": true, AuthToken: authToken, isAdmin: user.isAdmin, User: user });
+
+    } catch (err) {
+        return res.status(500).send("Internal Server error")
+    }
+})
+
+router.post('/getUser', authorize, async (req, res) => {
+
+    try {
+        const userID = req.user.id;
+        const user = await User.findById(userID).select('-password');
+        return res.send(user)
+    } catch (error) {
+        return res.status(500).send("Internal Server error")
+    }
+
+});
+
+router.post('/checkuser', authorize, async (req, res) => {
+
+    try {
+        const token = req.header('authToken');
+        return res.json({ "Success": true, "token": token })
+    } catch (error) {
+        return res.status(500).send("Internal Server error")
+    }
+
+});
+
+
 module.exports = router
