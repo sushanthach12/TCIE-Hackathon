bd48b7a72b46f94a9629d5c838c7a1d74dd7a1c8
Sushanth Acharya <84125038+sushanthach12@users.noreply.github.com>
2023-03-22 12:29:08 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AllRoutes from './Routes/AllRoutes';
 
 function App() {
   return (
-    <div className="mx-4 px-4 py-6">
+    <div className="mx-4 px-4 py-8 min-h-screen pb-20">
       <AllRoutes/>
     </div>
   );
